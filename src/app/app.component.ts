968c30b8d25d43c73fe626e15d4730ca79b6a727
andre_viallon <viallon.andre@gmail.com>
2021-02-06 15:09:35 +0100

    Added background image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <header></header>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header',
+  template: `
+    <div class="header-container"></div>
+  `,
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+}
