cff87bf99ceb9182a9ce1a57ddb9c0baa639bdbb
andre_viallon <viallon.andre@gmail.com>
2021-02-07 12:43:12 +0100

    add basic global stylings

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'header',
   template: `
-    <div class="header-container"></div>
+    <div class="header-container">
+      <h1>IP Address Tracker</h1>
+    </div>
   `,
   styleUrls: ['./header.component.scss']
 })
