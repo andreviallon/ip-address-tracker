90006e49c61b02e9563262430859e6b2cb120da4
André Viallon <viallon.andre@gmail.com>
2021-02-14 10:17:05 +0100

    WIP: small refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Dispatch } from "@ngxs-labs/dispatch-decorator";
+import { Select } from "@ngxs/store";
+import { Observable } from "rxjs";
+import { IpAddressState, IpAddress } from "./state/ip-address/ip-address.state";
+import { FetchIpAddress } from "./state/ip-address/ip-address.state.actions";
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <header-wrapper></header-wrapper>
-  `
+  selector: "app-root",
+  template: ` <header [ipAddress]="ipAddress$ | async"></header> `
 })
 export class AppComponent {
+  @Select(IpAddressState.ipAddress) ipAddress$: Observable<IpAddress>;
+  @Select(IpAddressState.loading) loading$: Observable<boolean>;
+
+  @Dispatch() fetchIpAddress = () => new FetchIpAddress("");
+
+  public ngOnInit(): void {
+    this.fetchIpAddress();
+  }
 }
